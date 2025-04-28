88b338e757e0225c32a43374837c34ab3fc438ca
Wafee Rahman <112278352+WafeeRahman@users.noreply.github.com>
2025-04-28 16:52:25 -0400

    Imported Smile Haar Cascade and Engineered Multiscale Parameters with Smile Cascade for Smile Detector

---
+++ Computer Vision/HappinessDetectorWithOpenCV.py
@@ -0,0 +1,75 @@
+# Facial Recognition Demo
+
+# Imports
+import cv2
+
+# Load in Cascades, these cascades will detect haar-like features. There are no neural nets required, since the XMLs contain the features we need, and are built into openCV
+# The cascade F(x) is a series of filters and weak features that we apply for detection (stored in xml)
+
+
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')  # Import facial feature cascade
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')  # Import facial feature cascade
+smile_cascade = cv2.CascadeClassifier('haarcascade_smile.xml')
+
+""" 
+Defining a function that detect faces through logitech camera.
+
+This function will draw squares around faces, aswell as detect eye features and faces
+
+Input - Face Image (Frames of Video Coming From Webcam) in grayscale. aswell as the frame of the image
+
+Output - Outline Image Ontop of Face 
+
+"""
+
+
+def detect(gray, frame):
+    # Import detectMultiScale function
+    # Pass in grayscale image, decrease it by 1.3x, and have acceptance criteria of 5 features in adaboost
+    # Experimental features - we find that these parameters work best for face detection through experimentation
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    # For the tuple sets given by faces
+    for x, y, w, h in faces:
+        # Draw rectangle over our face frame
+        # Coordinates of our face detected by multiscale filters
+        # Face Frame, Face Upper Left Coordinates, Bottom Right Coordinate, Frame Colour, thickness
+
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (255, 0, 0), 2)  # Prints Rectangle Into Frame
+
+        # Two regions of interest for eye features, one for the grayscale image passed in, and another for the original
+        # Region of Interest is the range between x, y and bottom right
+        roi_gray = gray[y:y + h, x:x + w]
+        roi_color = frame[y:y + h, x:x + w]
+
+        # Follow the same steps for eye cascade using the detectMultiScale provided by eye_cascade xml
+        # We pass in our region of interest instead, to save computation time and detect eyes exclusively within region of interest
+        eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 22)
+        
+        for ex, ey, ew, eh in eyes:
+            cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0),
+                          2)  # ROI Color Prints into Frame Aswell, as it works with the subsequence of frame
+            
+        #Within inner face and after eyes are detected, detect a smile w/ multiScale function given by cascade
+        smiles = smile_cascade.detectMultiScale(roi_gray, 1.7, 22) #Use smile cascade to detect smiles    
+        for sx, sy, sw, sh in smiles:
+            cv2.rectangle(roi_color, (sx, sy), (sx + sw, sy+sh), (0,0,255), 2) 
+    return frame
+
+
+# Connect to Camera and Feed Frames into Function
+video_capture = cv2.VideoCapture(0)  # Pass in EXT Logitech Webcam
+while True:
+    # Get Last Frame
+    _, frame = video_capture.read()
+    # Tranform to greyscale
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)  # Convert RGB to Gray
+    canvas = detect(gray, frame)
+    cv2.imshow('Video', canvas)  # Display to user
+
+    # If we keyboard input q, quit
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video_capture.release()
+cv2.destroyAllWindows()
